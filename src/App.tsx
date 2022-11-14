f08e4211e29487f071bc6cb27b240e0a0975b1d1
al-imam <mdalimam01828@gmail.com>
2022-11-14 18:44:10 +0600

    make hello world app
      - show hello world text
      - style it inline!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <div>noting here!</div>;
+  return (
+    <h1
+      style={{
+        textAlign: "center",
+        fontSize: "3rem",
+        marginTop: "64px",
+        color: "#111827",
+      }}
+    >
+      Hello World!
+    </h1>
+  );
 }
 
 export default App;
